6d709d5d256d47e6e0bd8b7d9c7805173aedb0d4
HCS Learning <learning@hardcybersoft.cl>
2021-02-24 16:53:49 -0300

    ejemplo de uso de libreria escrita con Typescript

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,9 @@
 import * as mylib from 'javascript-library'
+import * as tslib from 'typescript-library'
 
 export default (req, res) => {
   const resultado = mylib.restar(10, 2)
-  res.status(200).json({ operacion: '10 - 2 = x', resultado })
+  const division = "40 / 10 = x"
+  const resultadoDivision = tslib.dividir(40, 10)
+  res.status(200).json({ operacion: '10 - 2 = x', resultado, division, resultadoDivision })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import * as mylib from 'javascript-library'
+import * as tslib from 'typescript-library'
 
 export default function Home() {
   const resultado = mylib.sumar(1, 2)
+  const mul = tslib.multiplicar(3, 6)
   return (
+    <>
     <div>La suma de 1 + 2 = {resultado}</div>
+    <div>3 * 6 = {mul}</div>
+    </>
   )
 }
